e919d7769c43d8621c99f6a56cdc4f7e0cb33913
Scruf <ekozitski@hotmail.com>
2015-02-11 17:18:30 -0800

    testing

--- src/com/egor/employee/Source.java
+++ src/com/egor/employee/Source.java
@@ -15,5 +15,22 @@ public class Source {
         Employee emp1 = new Employee();
         Employee emp2 ;
         emp2 = new Employee("7789821234","Egor","Kozitski","01/06/1993","2363 81st","3475838019","software developer");
-/    }
+        System.out.println("Testing constructors by calling Print()");
+        System.out.println();
+        System.out.println("emp1 data after default constructor creation");
+        emp1.print();
+        System.out.println();
+        System.out.println("emp2 data after parameterized constructor");
+        emp2.print();
+        System.out.println();
+        System.out.printn("TESTING INSTANCE GETTER/SETTER METHODS(PROPERTIES)");
+        System.out.println();
+        System.out.println("Calling SETTER METHODS to SET DATA (Properties)");
+        emp1.setSSNumber("987675432");
+        emp1.setFirstName("Alex");
+        emp1.setLastName("Bob");
+        emp1.setDateOfBirth("2/21/1971");
+        emp1.setAddress("2276 bristol");
+        emp1.
+    }
 }
